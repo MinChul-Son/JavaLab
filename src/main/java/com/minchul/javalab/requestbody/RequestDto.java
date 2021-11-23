6ec472a24f562278d6d5a00cf63f72b952224f44
MinChul-Son <smc5236@naver.com>
2021-11-23 17:35:35 +0900

    `@RequestBody`는 setter가 필요없다!
    - Jackson2HttpMessageConverter가 값을 변환해주기 때문이다.

--- src/main/java/com/minchul/javalab/requestbody/RequestDto.java
+++ src/main/java/com/minchul/javalab/requestbody/RequestDto.java
@@ -9,13 +9,12 @@ public class RequestDto {
     private String name;
     private Long age;
 
-    private RequestDto(String name, Long age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public static RequestDto of(final String name, final Long age) {
-        return new RequestDto(name, age);
-    }
-
+//    private RequestDto(String name, Long age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public static RequestDto of(final String name, final Long age) {
+//        return new RequestDto(name, age);
+//    }
 }

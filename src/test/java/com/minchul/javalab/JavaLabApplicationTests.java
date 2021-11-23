526bcb4c8abf4e60eb434f9fdd7e1a04189de183
MinChul-Son <smc5236@naver.com>
2021-11-23 16:08:55 +0900

    프로젝트 생성

---
+++ src/test/java/com/minchul/javalab/JavaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minchul.javalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8f8f4326d36a3993da1ca8585052eaa6139baa97
MinChul-Son <smc5236@gmail.com>
2021-11-23 18:16:06 +0900

    `@RequestBody`의 값 바인딩 (#2)
    * `@ModelAttribute`용 컨트롤러 생성
    
    * `setter`가 없는 dto 클래스 생성
    
    * DTO 클래스에 setter 추가
    
    - `@ModelAttribute`는 프로퍼티 접근법을 사용한다.
    
    * DTO에 정적 팩토리 메서드 추가
    
    - `@ModelAttribute`를 쓸 때 DTO에 setter가 없어도 모든 필드를 매개변수로 받는 AllArgsConstructor가 있으면 바인딩 가능!
    
    * `@RequestBody`용 컨트롤로 생성
    
    * fix : Controller
    
    * `@RequestBody`는 setter가 필요없다!
    
    - Jackson2HttpMessageConverter가 값을 변환해주기 때문이다.

---
+++ src/main/java/com/minchul/javalab/requestbody/RequestDto.java
@@ -0,0 +1,20 @@
+package com.minchul.javalab.requestbody;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+//@Setter
+public class RequestDto {
+    private String name;
+    private Long age;
+
+//    private RequestDto(String name, Long age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public static RequestDto of(final String name, final Long age) {
+//        return new RequestDto(name, age);
+//    }
+}

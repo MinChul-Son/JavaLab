04b93834ea5fdaee42df70e69d2e294ec62cd93e
MinChul-Son <smc5236@naver.com>
2021-11-23 17:18:36 +0900

    DTO에 정적 팩토리 메서드 추가
    - `@ModelAttribute`를 쓸 때 DTO에 setter가 없어도 모든 필드를 매개변수로 받는 AllArgsConstructor가 있으면 바인딩 가능!

--- src/main/java/com/minchul/javalab/requestbody/RequestDto.java
+++ src/main/java/com/minchul/javalab/requestbody/RequestDto.java
@@ -4,8 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
+//@Setter
 public class RequestDto {
     private String name;
     private Long age;
+
+    private RequestDto(String name, Long age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static RequestDto of(final String name, final Long age) {
+        return new RequestDto(name, age);
+    }
+
 }

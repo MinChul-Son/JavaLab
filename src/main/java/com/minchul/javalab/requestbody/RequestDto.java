3d9cb131bc8fd187a48cdf9c4db63fa355edf3f1
MinChul-Son <smc5236@naver.com>
2021-11-23 17:13:22 +0900

    DTO 클래스에 setter 추가
    - `@ModelAttribute`는 프로퍼티 접근법을 사용한다.

--- src/main/java/com/minchul/javalab/requestbody/RequestDto.java
+++ src/main/java/com/minchul/javalab/requestbody/RequestDto.java
@@ -1,8 +1,10 @@
 package com.minchul.javalab.requestbody;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class RequestDto {
     private String name;
     private Long age;

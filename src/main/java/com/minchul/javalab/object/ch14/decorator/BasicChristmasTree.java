4f1302dba4aa372dcd862ebb836e8504027aff1f
MinChul-Son <smc5236@gmail.com>
2022-04-26 15:20:08 +0900

    feat: ConcreteComponent 구현

---
+++ src/main/java/com/minchul/javalab/object/ch14/decorator/BasicChristmasTree.java
@@ -0,0 +1,9 @@
+package com.minchul.javalab.object.ch14.decorator;
+
+public class BasicChristmasTree implements ChristmasTree{
+
+    @Override
+    public String decorator() {
+        return "Christmas Tree";
+    }
+}

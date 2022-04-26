3242265acf1667a6f6aceb52856a905a97c01a7a
MinChul-Son <smc5236@gmail.com>
2022-04-26 15:20:49 +0900

    feat: 클라이언트  구현

--- src/main/java/com/minchul/javalab/object/ch14/decorator/Client.java
+++ src/main/java/com/minchul/javalab/object/ch14/decorator/Client.java
@@ -5,17 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Client {
 
-    private final Server server;
+    public static void main(String[] args) {
+        ChristmasTree basicChristmasTree = new BasicChristmasTree();
+        log.info(basicChristmasTree.decorator());
 
-    public Client(Server server) {
-        this.server = server;
+        ChristmasTree bellTree = new Bell(basicChristmasTree);
+        log.info(bellTree.decorator());
+
+        ChristmasTree flowerBellTree = new Flower(bellTree);
+        log.info(flowerBellTree.decorator());
     }
 
-    public void request() {
-        log.info("요청을 서버로 전송합니다.");
-
-        server.process();
-
-        log.info("서버로부터 응답이 도착했습니다.");
-    }
 }
